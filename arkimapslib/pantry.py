36a4ac4990d695c78b77eba45fa423089aaff217
Enrico Zini <enrico@enricozini.org>
2021-03-17 18:03:29 +0100

    Don't fail to start if arkimet is not present: we can use eccodes. refs: #72

--- arkimapslib/pantry.py
+++ arkimapslib/pantry.py
@@ -65,97 +65,96 @@ class DiskPantry(inputs.InputStorage, PantryMixin):
         super().__init__(**kw)
 
 
-class ArkimetEmptyPantry(EmptyPantry):
-    """
-    Storage-less arkimet pantry only used to load recipes
-    """
-    def __init__(self, session: arkimet.dataset.Session, **kw):
-        super().__init__(**kw)
-        self.session = session
-
-    def add_input(self, inp: "inputs.Input"):
-        inp.compile_arkimet_matcher(self.session)
-        super().add_input(inp)
-
-
-class ArkimetPantry(DiskPantry):
-    """
-    Arkimet-based storage of GRIB files to be processed
-    """
-    def __init__(self, session: arkimet.dataset.Session, **kw):
-        super().__init__(**kw)
-        self.session = session
-
-    def add_input(self, inp: "inputs.Input"):
-        inp.compile_arkimet_matcher(self.session)
-        super().add_input(inp)
-
-    def fill(self, path: Optional[str] = None):
+if arkimet is not None:
+    class ArkimetEmptyPantry(EmptyPantry):
         """
-        Read data from standard input and acquire it into the pantry
+        Storage-less arkimet pantry only used to load recipes
         """
-        # Create pantry dir if missing
-        os.makedirs(self.data_root, exist_ok=True)
+        def __init__(self, session: "arkimet.dataset.Session", **kw):
+            super().__init__(**kw)
+            self.session = session
 
-        # Dispatch todo-list
-        match: List[Tuple[Any, str]] = []
-        for inps in self.inputs.values():
-            for inp in inps:
-                matcher = getattr(inp, "arkimet_matcher", None)
-                if matcher is None:
-                    log.info("%s (model=%s): skipping input with no arkimet matcher filter", inp.name, inp.model)
-                    continue
-                match.append((matcher, inp.pantry_basename))
+        def add_input(self, inp: "inputs.Input"):
+            inp.compile_arkimet_matcher(self.session)
+            super().add_input(inp)
 
-        dispatcher = ArkimetDispatcher(self.data_root, match)
-        with self.open_dispatch_input(path=path) as infd:
-            dispatcher.read(infd)
+    class ArkimetPantry(DiskPantry):
+        """
+        Arkimet-based storage of GRIB files to be processed
+        """
+        def __init__(self, session: arkimet.dataset.Session, **kw):
+            super().__init__(**kw)
+            self.session = session
 
+        def add_input(self, inp: "inputs.Input"):
+            inp.compile_arkimet_matcher(self.session)
+            super().add_input(inp)
 
-class ArkimetDispatcher:
-    """
-    Read a stream of arkimet metadata and store its data into a dataset
-    """
-    def __init__(self, data_root: str, todo_list: List[Tuple[Any, str]]):
-        self.data_root = data_root
-        self.todo_list = todo_list
+        def fill(self, path: Optional[str] = None):
+            """
+            Read data from standard input and acquire it into the pantry
+            """
+            # Create pantry dir if missing
+            os.makedirs(self.data_root, exist_ok=True)
 
-    def dispatch(self, md: arkimet.Metadata) -> bool:
-        for matcher, pantry_basename in self.todo_list:
-            if matcher.match(md):
-                trange = md.to_python("timerange")
-                style = trange["style"]
-                if style == "GRIB1":
-                    if trange['trange_type'] in (0, 1):
-                        output_step = trange['p1']
-                    elif trange['trange_type'] in (2, 3, 4, 5, 6, 7):
-                        output_step = trange['p2']
-                    else:
-                        log.warning("unsupported timerange %s: skipping input", trange)
+            # Dispatch todo-list
+            match: List[Tuple[Any, str]] = []
+            for inps in self.inputs.values():
+                for inp in inps:
+                    matcher = getattr(inp, "arkimet_matcher", None)
+                    if matcher is None:
+                        log.info("%s (model=%s): skipping input with no arkimet matcher filter", inp.name, inp.model)
                         continue
-                elif style == "Timedef":
-                    output_step = trange["step_len"]
-                else:
-                    log.warning("unsupported timerange style in %s: skipping input", trange)
-                    continue
+                    match.append((matcher, inp.pantry_basename))
 
-                source = md.to_python("source")
+            dispatcher = ArkimetDispatcher(self.data_root, match)
+            with self.open_dispatch_input(path=path) as infd:
+                dispatcher.read(infd)
 
-                dest = os.path.join(self.data_root, pantry_basename + f"+{output_step}.{source['format']}")
-                # TODO: implement Metadata.write_data to write directly without
-                # needing to create an intermediate python bytes object
-                with open(dest, "ab") as out:
-                    out.write(md.data)
-        return True
-
-    def read(self, infd: BinaryIO):
+    class ArkimetDispatcher:
         """
-        Read data from an input file, or standard input.
-
-        The input file is the output of arki-query --inline, which is the same
-        as is given as input to arkimet processors.
+        Read a stream of arkimet metadata and store its data into a dataset
         """
-        arkimet.Metadata.read_bundle(infd, dest=self.dispatch)
+        def __init__(self, data_root: str, todo_list: List[Tuple[Any, str]]):
+            self.data_root = data_root
+            self.todo_list = todo_list
+
+        def dispatch(self, md: arkimet.Metadata) -> bool:
+            for matcher, pantry_basename in self.todo_list:
+                if matcher.match(md):
+                    trange = md.to_python("timerange")
+                    style = trange["style"]
+                    if style == "GRIB1":
+                        if trange['trange_type'] in (0, 1):
+                            output_step = trange['p1']
+                        elif trange['trange_type'] in (2, 3, 4, 5, 6, 7):
+                            output_step = trange['p2']
+                        else:
+                            log.warning("unsupported timerange %s: skipping input", trange)
+                            continue
+                    elif style == "Timedef":
+                        output_step = trange["step_len"]
+                    else:
+                        log.warning("unsupported timerange style in %s: skipping input", trange)
+                        continue
+
+                    source = md.to_python("source")
+
+                    dest = os.path.join(self.data_root, pantry_basename + f"+{output_step}.{source['format']}")
+                    # TODO: implement Metadata.write_data to write directly without
+                    # needing to create an intermediate python bytes object
+                    with open(dest, "ab") as out:
+                        out.write(md.data)
+            return True
+
+        def read(self, infd: BinaryIO):
+            """
+            Read data from an input file, or standard input.
+
+            The input file is the output of arki-query --inline, which is the same
+            as is given as input to arkimet processors.
+            """
+            arkimet.Metadata.read_bundle(infd, dest=self.dispatch)
 
 
 class EccodesPantry(DiskPantry):
