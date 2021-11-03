d29ea1417f282a7cad480a31e3d2067693f6ecd0
Enrico Zini <enrico@enricozini.org>
2021-11-03 18:56:04 +0100

    Deal with rh2m computing inputs differently for model erg5. refs: #52

--- tests/test_rh2m.py
+++ tests/test_rh2m.py
@@ -4,12 +4,15 @@ from arkimapslib.unittest import add_recipe_test_cases
 
 class RH2MMixin:
     def test_dispatch(self):
-        self.fill_pantry()
+        self.fill_pantry(expected=[f'{self.model_name}_2d+12.grib', f'{self.model_name}_t2m+12.grib'])
 
         # Check that the right input was selected
         cpdec3h = self.kitchen.pantry.inputs.get("rh2m")
         for i in cpdec3h:
-            self.assertEqual(i.__class__.__name__, "VG6DTransform")
+            if i.model == "erg5":
+                self.assertEqual(i.__class__.__name__, "Source")
+            else:
+                self.assertEqual(i.__class__.__name__, "VG6DTransform")
 
         orders = self.make_orders()
         self.assertEqual(len(orders), 1)
