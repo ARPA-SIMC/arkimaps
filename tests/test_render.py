3a13f588c7f57c3b0548367f6ec2ba7febb034ad
Enrico Zini <enrico@enricozini.org>
2021-11-09 13:19:56 +0100

    Added test for #83 with an explanatory message. Refs: #83

---
+++ tests/test_render.py
@@ -0,0 +1,47 @@
+# from __future__ import annotations
+import os
+import tempfile
+import unittest
+from arkimapslib.render import Renderer
+
+
+class TestRender(unittest.TestCase):
+    def test_issue83(self):
+        with tempfile.TemporaryDirectory() as workdir:
+            renderer = Renderer(workdir)
+            with renderer.override_env():
+                from Magics import macro
+                import Magics
+                output = macro.output(output_formats=['png'], output_name=os.path.join(workdir, "issue83"))
+                parts = []
+                parts.append(macro.mmap(**{
+                    'subpage_map_projection': 'mercator',
+                    'subpage_lower_left_longitude': 5.1,
+                    'subpage_lower_left_latitude': 43.0,
+                    'subpage_upper_right_longitude': 15.0,
+                    'subpage_upper_right_latitude': 47.5,
+                    'subpage_map_vertical_longitude': 10.3,
+                }))
+                parts.append(macro.msymb(**{
+                    'symbol_type': 'marker',
+                    'symbol_marker_index': 15,
+                    'legend': 'off',
+                    'symbol_colour': 'black',
+                    'symbol_height': 0.28}))
+
+                has_issue83 = False
+                try:
+                    macro.plot(output, *parts)
+                except Magics.Magics.MagicsError as e:
+                    if "ProjP: cannot create crs to crs" in str(e):
+                        has_issue83 = True
+                    else:
+                        raise
+
+                if has_issue83:
+                    self.fail(
+                        "this system seems to have trouble locating proj.db. See"
+                        " https://github.com/ARPA-SIMC/arkimaps/issues/83 or"
+                        " https://www.enricozini.org/blog/2021/debian/an-educational-debugging-session/"
+                        " for details, and set PROJ_LIB=/usr/share/proj (or the"
+                        " equivalent path in your system) as a workaround")
