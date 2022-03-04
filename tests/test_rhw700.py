f2cde53e0e4290c44540f4ded89d9f7b5b2938bf
Enrico Zini <enrico@enricozini.org>
2022-03-04 16:12:09 +0100

    Removed redundant processing. Refs: #94

--- tests/test_rhw700.py
+++ tests/test_rhw700.py
@@ -19,13 +19,9 @@ class RHW700Mixin:
             f'{self.model_name}_v700_2021_1_10_0_0_0+12.grib'
             ' uv700_2021_1_10_0_0_0+12.grib',
             'p700:VG6DTransform:vg6d_transform --comp-var-from-lev --trans-level-type=100 -'
-            ' cosmo_p700_2021_1_10_0_0_0+12.grib',
-            'p700:VG6DTransform:vg6d_transform --comp-var-from-lev --trans-level-type=100 -'
-            ' ifs_p700_2021_1_10_0_0_0+12.grib',
+            ' p700_2021_1_10_0_0_0+12.grib',
             'rh700:VG6DTransform:vg6d_transform --output-variable-list=B13003 -'
-            ' cosmo_rh700_2021_1_10_0_0_0+12.grib',
-            'rh700:VG6DTransform:vg6d_transform --output-variable-list=B13003 -'
-            ' ifs_rh700_2021_1_10_0_0_0+12.grib',
+            ' rh700_2021_1_10_0_0_0+12.grib',
         ])
 
         self.assertRenders(orders[0])
