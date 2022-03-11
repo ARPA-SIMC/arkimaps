3ad738fe6b32d8a257d775b5644838482f7b6b25
Enrico Zini <enrico@enricozini.org>
2022-03-11 11:57:58 +0100

    Start a test case for Cosmo without convective snow. refs: #24

--- tests/test_sf3h.py
+++ tests/test_sf3h.py
@@ -28,6 +28,33 @@ class SF3HCosmoMixin:
         self.assertMgribArgsEqual(orders[0], cosmo={}, ifs={})
 
 
+class SF3HNoConvMixin:
+    def test_dispatch(self):
+        self.maxDiff = None
+        pantry_reftime = "2021_1_10_0_0_0"
+        self.fill_pantry(
+            expected=[
+                f"{self.model_name}_snowgsp_{pantry_reftime}+{step}.grib"
+                for step in range(0, 13)],
+            exclude="*_snowcon_*")
+
+        orders = self.make_orders()
+        self.assertGreaterEqual(len(orders), 4)
+        orders = [o for o in orders if o.basename == "sf3h+012"]
+        self.assertEqual(len(orders), 1)
+
+        self.assertProcessLogEqual(
+            ["snowsum:VG6DTransform:vg6d_transform --output-variable-list=B13205 -"
+             f" cosmo_snowsum_2021_1_10_0_0_0+{step}.grib" for step in range(0, 13)] +
+            ["snowdec3h:Decumulate:vg6d_transform --comp-stat-proc=1 '--comp-step=0 03'"
+             " --comp-frac-valid=0 --comp-full-steps - cosmo_snowdec3h-decumulated.grib"]
+        )
+
+        self.assertRenders(orders[0])
+
+        self.assertMgribArgsEqual(orders[0], cosmo={}, ifs={})
+
+
 class SF3HIFSMixin:
     def test_dispatch(self):
         pantry_reftime = "2021_1_10_0_0_0"
@@ -47,7 +74,6 @@ class SF3HIFSMixin:
 
         self.assertMgribArgsEqual(orders[0], cosmo={}, ifs={})
 
-# TODO: test the case without convective snowfall
-
 
 add_recipe_test_cases(__name__, "sf3h")
+add_recipe_test_cases(__name__, "sf3h", models=["Cosmo"], test_mixin=SF3HNoConvMixin)
