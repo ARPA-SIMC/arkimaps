25330a6c1f8e2df21d2400789b652ffe1b00410e
Enrico Zini <enrico@enricozini.org>
2022-03-28 11:39:16 +0200

    Added initial draft recipe and test case, which fails with IFS. refs: #5

---
+++ tests/test_alt0.py
@@ -0,0 +1,32 @@
+# from __future__ import annotations
+from arkimapslib.unittest import add_recipe_test_cases
+
+
+class ALT0CosmoMixin:
+    def test_process(self):
+        self.fill_pantry(expected=["cosmo_alt0_2021_1_10_0_0_0+12.grib"])
+
+        orders = self.make_orders()
+        self.assertEqual(len(orders), 1)
+
+        self.assertRenders(orders[0])
+
+        self.assertMgribArgsEqual(orders[0], cosmo={}, ifs={})
+
+
+class ALT0IFSMixin:
+    def test_process(self):
+        self.fill_pantry(expected=[
+            "ifs_alt0ground_2021_1_10_0_0_0+12.grib",
+            "ifs_orography_2021_1_10_0_0_0+0.grib",
+        ])
+
+        orders = self.make_orders()
+        self.assertEqual(len(orders), 1)
+
+        self.assertRenders(orders[0])
+
+        self.assertMgribArgsEqual(orders[0], cosmo={}, ifs={})
+
+
+add_recipe_test_cases(__name__, "alt0")
