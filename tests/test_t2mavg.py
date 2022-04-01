09dba5b96550a934fe2ba5bb89c5b5b9f91e3351
Enrico Zini <enrico@enricozini.org>
2022-04-01 15:33:02 +0200

    Use average for t2mavg and added test. refs: #109

---
+++ tests/test_t2mavg.py
@@ -0,0 +1,22 @@
+# from __future__ import annotations
+from arkimapslib.unittest import add_recipe_test_cases
+
+
+class T2MAVGMixin:
+    def test_dispatch(self):
+        self.fill_pantry()
+
+        orders = self.make_orders()
+        self.assertEqual(len(orders), 1)
+
+        self.assertProcessLogEqual([
+            "t2mavg:Average:vg6d_transform --comp-frac-valid=0 '--comp-step=0 24'"
+            " --comp-stat-proc=254:0 - t2mavg-decumulated.grib",
+        ])
+
+        self.assertRenders(orders[0], step=24)
+
+        self.assertMgribArgsEqual(orders[0], cosmo={}, ifs={})
+
+
+add_recipe_test_cases(__name__, "t2mavg")
