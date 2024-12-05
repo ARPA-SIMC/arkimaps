9a302df2e4a4573d1c415ceb52a9dfbfd873a9bb
Emanuele Di Giacomo <edigiacomo@arpae.it>
2024-12-05 15:21:56 +0100

    assertEqual instead of assertEquals

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -9,19 +9,19 @@ class TestSetdefaultDeep(unittest.TestCase):
     def test_empty(self):
         d = {}
         setdefault_deep(d, {})
-        self.assertEquals(d, {})
+        self.assertEqual(d, {})
 
     def test_set(self):
         d = {}
         setdefault_deep(d, {"a": 1, "b": {"c": 2}})
-        self.assertEquals(d, {"a": 1, "b": {"c": 2}})
+        self.assertEqual(d, {"a": 1, "b": {"c": 2}})
 
     def test_merge(self):
         d = {"a": 1}
         setdefault_deep(d, {"a": 2, "b": 3})
-        self.assertEquals(d, {"a": 1, "b": 3})
+        self.assertEqual(d, {"a": 1, "b": 3})
 
     def test_merge_deep(self):
         d = {"a": 1, "b": {"c": 1, "d": 2}}
         setdefault_deep(d, {"a": 2, "b": {"e": 3, "c": 2}})
-        self.assertEquals(d, {"a": 1, "b": {"c": 1, "d": 2, "e": 3}})
+        self.assertEqual(d, {"a": 1, "b": {"c": 1, "d": 2, "e": 3}})
